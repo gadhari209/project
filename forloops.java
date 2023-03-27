8f149f14fcf55938fa3b16c31d556e65662a8c3a
gadhari209 <129051665+gadhari209@users.noreply.github.com>
2023-03-27 15:19:21 +0530

    java notes
    for loops program

---
+++ forloops.java
@@ -0,0 +1,7 @@
+public class forloops {
+    public static void main(String arg[]){
+        for (int counter= 0; counter<5; counter=counter+1){
+            System.out.println("Hello world");
+        }
+    }
+}
