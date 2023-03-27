aaf85b4388d1ff4762c9024faa8cbeab74d2cc60
gadhari209 <129051665+gadhari209@users.noreply.github.com>
2023-03-27 17:29:30 +0530

    Hello java

---
+++ Hello.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class Hello
+{ public static void main(String array[])
+{  
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int n=1;
+Switch(n)
+{
+    case 1: 
+    System.out.println("jan");
+    break;
+    case 2:
+    System.out.println ("feb");
+}
+
+    }
+
+    }
+
