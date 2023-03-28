d15e0ba9720f7e524906ac175a0edba90c98d535
gadhari209 <129051665+gadhari209@users.noreply.github.com>
2023-03-28 16:12:28 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,12 @@
+public class pattern {
+    public static void main(String arg[]){
+        int n = 4;
+        int m = 5;
+        for (int i= 0; i<n; i=i+1){
+            for ( int j=1 ; j<m;j++ ){
+                 System.out.print("*");
+                    }
+                System.out.println();
+        }
+    }
+} 
